2538413cf8ff231f4ee39dc712a8210cb94d61f3
vibesharing <buret.hadrien@orange.fr>
2016-07-09 14:25:06 +0200

    Css + working map

--- Rally/www/js/app.js
+++ Rally/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ngCordova', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -49,7 +49,24 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     }
   })
-
+  .state('tab.description',{
+    url:'/description/:parcourId',
+    views: {
+      'tab-description':{
+        templateUrl: 'templates/tab-description.html',
+        controller: 'parcourCtrl'
+      }
+    }
+  })
+  .state('tab.map',{
+    url:'/map',
+    views: {
+      'tab-map':{
+        templateUrl: 'templates/tab-map.html',
+        controller: 'mapCtrl'
+      }
+    }
+  })
   .state('tab.chats', {
       url: '/chats',
       views: {
--- Rally/www/js/controllers/mapCtrl.js
+++ Rally/www/js/controllers/mapCtrl.js
@@ -1,35 +1,79 @@
-function mapCtrl($rootScope,$scope,$stateParams, $state,$http, $cordovaGeolocation){
-  var options = {timeout: 10000, enableHighAccuracy: true};
+function mapCtrl($scope, $state,$http, $cordovaGeolocation) {
+    var options = {timeout: 10000, enableHighAccuracy: true};
 
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-    var mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-      $http.get('http://192.168.3.196:8000/users').then(function(res){
-        $scope.listProfile = res.data;
-        for(var i =0; i < $scope.listProfile.length; i++){
-          var image = {
-            url: '../../img/'+$scope.listProfile[i].Firstname+'.jpg',
-            size: new google.maps.Size(200, 320),
-            origin: new google.maps.Point(0, 0)
-          };
-          var title_name = $scope.listProfile[i].Firstname;
-          new google.maps.Marker({
+    //  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+     //
+    //    var latLng = position;
+    //
+    //    var mapOptions = {
+    //      center: latLng,
+    //      zoom: 15,
+    //      mapTypeId: google.maps.MapTypeId.ROADMAP
+    //    };
+    //
+    //    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+     //
+    //  }, function(error){
+    //    console.log("Could not get location");
+    //  });
+    //
+    $scope.addMyPosition = function(map){
+      $scope.map = map;
+    new google.maps.Marker({
               map: $scope.map,
               animation: google.maps.Animation.DROP,
-              position: $scope.listProfile[i].LatLng,
+              position: {
+                    "lat": 41.5507300,
+                    "lng": -87.6512600
+                },
+              icon:'https://image.freepik.com/icones-gratuites/gps-symbole-de-localisation_318-76275.jpg'
           });
-        }
-      });
-    });
+        };
+    //
+    //
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var map;
+  $scope.initialize = function(){
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        var inticor= {
+              "lat": 41.8507300,
+              "lng": -87.6512600
+          };
+        var mapOptions =
+                {
+                    zoom: 9,
+                    center: inticor,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                };
+
+        map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        $scope.addMyPosition(map);
+        directionsDisplay.setMap(map);
+        calcRoute();
+
+    };
+// google.maps.event.addDomListener(window, 'load', $scope.initialize());
+
+function calcRoute() {
+  var start = {
+        "lat": 41.8507300,
+        "lng": -87.6512600
+    };
+  var end = {
+        "lat": 40.8507300,
+        "lng": -87.6512600
+    };
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
   });
+}
+$scope.initialize();
 }
